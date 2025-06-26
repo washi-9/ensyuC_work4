ae0dc07bcf008276d49f1e141212a87c19e843c3
Koichiro <tokails188s@gmail.com>
2025-06-26 22:23:41 +0900

    it works

---
+++ chatclient.c
@@ -0,0 +1,142 @@
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<netdb.h>
+#include<unistd.h>
+#include<string.h>
+#include<stdio.h>
+#include<stdlib.h>
+
+#define PORT 10140
+#define BUFFER_SIZE 1024
+#define MAXCLIENTS 5
+
+int state = 1;
+
+int main(int argc, char **argv) {
+    int sock;
+    struct sockaddr_in ServAddr;
+    struct hostent *hp;
+    char *servIP;
+    char rbuf[BUFFER_SIZE];
+    int bytesRcvd;
+    struct timeval tv;
+    fd_set rfds;
+
+    if (argc != 3) {
+        fprintf(stderr, "Usage: %s <host> <username>\n", argv[0]);
+        exit(1);
+    }
+
+    
+    if ((sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {
+        perror("socket() failed");
+        exit(1);
+    }
+    
+    servIP = argv[1];
+    
+    if ((hp = gethostbyname(servIP)) == NULL) {
+        perror("gethostbyname() failed");
+        exit(1);
+    }
+
+    memset(&ServAddr, 0, sizeof(ServAddr));
+    ServAddr.sin_family = AF_INET;
+    memcpy(&ServAddr.sin_addr, hp->h_addr, hp->h_length);
+    ServAddr.sin_port = htons(PORT);
+
+    if (connect(sock, (struct sockaddr *) &ServAddr, sizeof(ServAddr)) < 0) {
+        perror("connect() failed");
+        close(sock);
+        exit(1);
+    }
+
+    printf("state 1 finished\n");
+    // state 2 start
+    state = 2;
+
+    read(sock, rbuf, BUFFER_SIZE);
+    if (strncmp(rbuf, "REQUEST ACCEPTED\n", 17) != 0) {
+        fprintf(stderr, "Server did not accept the connection.\n");
+        state = 6;
+        exit(1);
+    }
+
+    printf("state 2 finished\n");
+    // state 3 start
+    state = 3;
+
+    write(sock, argv[2], strlen(argv[2]));
+    read(sock, rbuf, BUFFER_SIZE);
+    if (strncmp(rbuf, "USERNAME REGISTERED\n", 20) != 0) {
+        fprintf(stderr, "Server did not register the username.\n");
+        state = 6;
+        exit(1);
+    }
+
+    printf("state 3 finished\n");
+    // state 4 start
+    state = 4;
+
+    while(state == 4) {
+        FD_ZERO(&rfds);
+        FD_SET(0, &rfds);
+        FD_SET(sock, &rfds);
+
+        int maxfd = sock > 0 ? sock : 0;
+
+        tv.tv_sec = 1;
+        tv.tv_usec = 0;
+
+        if (select(maxfd + 1, &rfds, NULL, NULL, &tv) > 0) {
+            if (FD_ISSET(0, &rfds)) {
+                // Read from stdin
+                if (fgets(rbuf, BUFFER_SIZE, stdin) != NULL) {
+                    size_t len = strlen(rbuf);
+                    if (rbuf[0] == EOF) {
+                        state = 5;
+                        break;
+                    }
+                    if (write(sock, rbuf, len) != len) {
+                        perror("write() failed");
+                        break;
+                    }
+                } else {
+                    if (feof(stdin)) {
+                        state = 5;
+                        break;
+                    } else {
+                        perror("fgets() failed");
+                        state = 6;
+                        break;
+                    }
+                }
+            }
+
+            if (FD_ISSET(sock, &rfds)) {
+                // Read from socket
+                bytesRcvd = read(sock, rbuf, BUFFER_SIZE - 1);
+                if (bytesRcvd <= 0) {
+                    perror("read() failed or connection closed");
+                    state = 6;
+                    break;
+                }
+                rbuf[bytesRcvd] = '\0';
+                printf("%s", rbuf);
+            }
+        }
+    }
+
+    printf("state 4 finished\n");
+    // state 5 start
+    if (state == 5) {
+        close(sock);
+        printf("Exit chatclient\n");
+        exit(0);
+    } else if (state == 6) {
+        close(sock);
+        printf("Error occurred\n");
+        exit(1);
+    }
+}
